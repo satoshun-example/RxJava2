14df18ce4ae7d1eaa3584ed1b34ca37c51560c99
SatoShun <shun.sato1@gmail.com>
2016-12-17 08:03:37 +0000

    first TestObserver, assert complete and values

--- src/test/java/com/github/satoshun/example/io/reactivex/ObservableTest.java
+++ src/test/java/com/github/satoshun/example/io/reactivex/ObservableTest.java
@@ -1,12 +1,14 @@
 package com.github.satoshun.example.io.reactivex;
 
+import com.github.satoshun.example.io.reactivex.observers.TestObserver;
 import org.junit.Test;
 
 public class ObservableTest {
 
   @Test public void just() throws Exception {
-    // todo use test observer
-    Observable.just("test")
-        .subscribe();
+    TestObserver<String> observer = Observable.just("test")
+        .test().await();
+    observer.assertValues("test");
+    observer.assertComplete();
   }
 }
